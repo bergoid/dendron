f2736b2897524f248a09f7e1e463941fc0d9131c
bergoid <bergoid@users.noreply.github.com>
2022-01-24 22:03:52 +0100

    Page layout with L sticky sidebar

--- samples/page-layout/ssb.js
+++ samples/page-layout/ssb.js
@@ -2,54 +2,57 @@
 // https://github.com/Krzysztof-Antosik/Two-direction-Sticky-Sidebar
 
 //aside selector
-const aside = document.querySelector('[data-sticky="true"]'),
+const aside = document.querySelector(".stickySidebar");
 
-//variables
-startScroll = 0;
+// variables
+// startScroll = 0;
 var endScroll = window.innerHeight - aside.offsetHeight;// -500;
-currPos = window.scrollY;
-screenHeight = window.innerHeight;
-asideHeight = aside.offsetHeight;
-aside.style.top = startScroll + 'px';
+var currPos = window.scrollY;
+// screenHeight = window.innerHeight;
+// asideHeight = aside.offsetHeight;
+// aside.style.top = startScroll + 'px';
+aside.style.top = '0px';
 
-//check height screen and aside on resize
-window.addEventListener(
-    'resize',
-    () => {
-        screenHeight = window.innerHeight;
-        asideHeight = aside.offsetHeight;
-    }
-);
+// check height screen and aside on resize
+// window.addEventListener(
+//     'resize',
+//     () => {
+//         screenHeight = window.innerHeight;
+//         asideHeight = aside.offsetHeight;
+//     }
+// );
 
-//main function
+// main function
 document.addEventListener(
     'scroll',
     () => {
         endScroll = window.innerHeight - aside.offsetHeight;
         let asideTop = parseInt(aside.style.top.replace('px;', ''));
 
-        if (asideHeight>screenHeight)
+//         if (asideHeight > screenHeight)
+        if (aside.offsetHeight > window.innerHeight)
         {
             if (window.scrollY < currPos)
             {
-                //scroll up
-                if (asideTop < startScroll)
+                // scroll up
+                if (asideTop < 0)
                 {
+                    console.log("asideTop == " + asideTop);
                     aside.style.top = (asideTop + currPos - window.scrollY) + 'px';
                 }
-                else if (asideTop >= startScroll && asideTop != startScroll)
+                else if (asideTop > 0)
                 {
-                    aside.style.top = startScroll + 'px';
+                    aside.style.top = '0px';
                 }
             }
             else
             {
-                //scroll down
+                // scroll down
                 if (asideTop > endScroll)
                 {
                     aside.style.top = (asideTop + currPos - window.scrollY) + 'px';
                 }
-                else if (asideTop < (endScroll) && asideTop != endScroll)
+                else if (asideTop < endScroll)
                 {
                     aside.style.top = endScroll + 'px';
                 }
@@ -57,7 +60,7 @@ document.addEventListener(
         }
         else
         {
-            aside.style.top = startScroll + 'px';
+            aside.style.top = '0px';
         }
 
         currPos = window.scrollY;
