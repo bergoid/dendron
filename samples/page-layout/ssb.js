1a7dfdda98089dbf33811d7afa93e4228e66ecf6
bergoid <bergoid@users.noreply.github.com>
2022-06-05 21:54:47 +0200

    fixed panel above ssb

--- samples/page-layout/ssb.js
+++ samples/page-layout/ssb.js
@@ -1,37 +1,28 @@
 // Sticky sidebar
-// Adapted from:
-// https://github.com/Krzysztof-Antosik/Two-direction-Sticky-Sidebar
 
-//aside selector
-const aside = document.querySelector('.sticky-sidebar');
+// HTML elements
+const ssb_fixed = document.querySelector('.sticky-sidebar > .ssb-fixed');
+const ssb_content = document.querySelector('.sticky-sidebar > .ssb-content');
+
+// Align ssb_content and ssb_fixed
+ssb_content.style.marginTop = ssb_fixed.offsetHeight + 'px';
+ssb_fixed.style.width = ssb_content.offsetWidth + 'px';
 
 // Keep track of previous scroll position
 var previousScrollY = window.scrollY;
-
-// Set style.top explicitly
-aside.style.top = '0px';
-
-console.log("aside.style.top == " + aside.style.top);
 
 // onScroll event handler
 document.addEventListener(
     'scroll',
     () => {
-        // Only do stuff if sidebar is too high to fit in viewport
-        if (aside.offsetHeight > window.innerHeight)
-        {
-            let currentTop = parseInt(aside.style.top.replace('px;', ''));
 
-            // Scrolling up
-            if (window.scrollY < previousScrollY)
-                aside.style.top = Math.min(0, currentTop + previousScrollY - window.scrollY) + 'px';
+        // Scrolling up
+        if (window.scrollY < previousScrollY)
+            ssb_content.style.top = Math.max(0, Math.min(parseInt(ssb_content.style.top.replace('px;', '')), window.scrollY)) + 'px';
 
-            // Scrolling down
-            else
-                aside.style.top = Math.max(window.innerHeight - aside.offsetHeight, currentTop + previousScrollY - window.scrollY) + 'px';
-
-            console.log("(onscroll) aside.style.top == " + aside.style.top);
-        }
+        // Scrolling down
+        else
+            ssb_content.style.top = Math.max(0, window.scrollY + window.innerHeight - ssb_content.offsetHeight - ssb_fixed.offsetHeight) + 'px';
 
         previousScrollY = window.scrollY;
     },
@@ -40,3 +31,5 @@ document.addEventListener(
         passive: true
     }
 );
+
+// TODO : onResize handler
